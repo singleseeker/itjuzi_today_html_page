5bad22741a9862a787049aecc8c6e7ca8225ebd8
SingleSeeker <671826@qq.com>
2014-10-10 15:20:07 +0800

    Fix product comment.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,10 @@
-console.log('\'Allo \'Allo!');
+$(function(){
+
+	$('#show_all_comment').on('shown.bs.modal', function () {
+	    $('.modal-content').scrollTop(0);
+	    $('body').css({overflow: 'hidden'}); 
+	}).on('hidden.bs.modal', function () {
+	    $('body').css({overflow: ''}); 
+	});
+
+});
