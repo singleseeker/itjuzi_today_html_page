486425f9231506d60f25dca8a7bec62677b3e0bd
SingleSeeker <671826@qq.com>
2014-10-11 14:11:58 +0800

    Add index ajax content.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,52 @@
-$(function(){
+'use strict';
 
+$(function(){
+	
 	$('#show_all_comment').on('shown.bs.modal', function () {
-	    $('.modal-content').scrollTop(0);
-	    $('body').css({overflow: 'hidden'}); 
+		$('.modal-content').scrollTop(0);
+		$('body').css({overflow: 'hidden'});
 	}).on('hidden.bs.modal', function () {
-	    $('body').css({overflow: ''}); 
+		$('body').css({overflow: ''});
+	});
+
+	$(document).on('click', '.comment', function () {
+		
+		var itemId = $(this).attr('item-id');
+		var request = $.ajax({
+			url: 'ajax_item.html',
+			type: 'GET',
+			data: { id : itemId },
+			dataType: 'html'
+		});
+
+		request.done(function( msg ) {
+			// get number of like
+			$('#show_all_comment').html(msg);
+		});
+
+	});
+
+	// click like button
+	$(document).on('click', '.like', function(){
+		var base = $(this);
+
+		var itemId = base.attr('item-id');
+		var request = $.ajax({
+			url: 'ajax_item.html',
+			type: 'GET',
+			data: { id : itemId },
+			dataType: 'html'
+		});
+		 
+		request.done(function( msg ) {
+			// get number of like
+			msg = 8;
+
+			base.find('span').addClass('liked');
+			base.find('strong').text(msg);
+		});
+
+		return false;
 	});
 
 });
